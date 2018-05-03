65cfe082b4b59dd1858a4c015207fdade10615c2
DESKTOP-CASPP43\Guillermo Morillo <guillermo.morillo@bluu.es>
2018-05-03 10:10:33 +0200

    set ChromeDriver

---
+++ src/test/Utils/ChromeDriverBase.java
@@ -0,0 +1,39 @@
+package test.Utils;
+
+import org.openqa.selenium.WebDriver;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import java.util.HashMap;
+import java.util.concurrent.TimeUnit;
+
+public class ChromeDriverBase {
+    protected static WebDriver driver;
+
+    @BeforeClass
+    public static void init(){
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\Guillermo Morillo\\Documents\\chromedriver1.exe");
+
+        HashMap<String, Object> chromePrefs = new HashMap<String, Object>();
+        chromePrefs.put("download.default_directory", System.getProperty("user.dir") + "\\Downloads");
+        chromePrefs.put("download.prompt_for_download", false);
+        chromePrefs.put("download.directory_upgrade", true);
+        chromePrefs.put("safebrowsing.enabled", true);
+
+        ChromeOptions options = new ChromeOptions();
+        //options.addArguments("--headless");
+        options.setExperimentalOption("prefs", chromePrefs);
+
+        driver = new ChromeDriver(options);
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
+        driver.get("http://wheretoeat-ca57a.firebaseapp.com/");
+    }
+
+    @AfterClass
+    public static void finishIt(){
+        driver.manage().deleteAllCookies();
+        driver.close();
+    }
+}
---
+++ src/test/tests/MainTest.java
@@ -0,0 +1,10 @@
+package test.tests;
+
+import org.junit.Test;
+import test.Utils.ChromeDriverBase;
+
+public class MainTest extends ChromeDriverBase {
+ @Test
+ public void test () {
+ }
+}
